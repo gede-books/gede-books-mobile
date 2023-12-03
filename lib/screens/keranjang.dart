a2116fc8362ef71388b5dfa6b44e9ee9eb214edc
adhan-857 <adhan.andika857@gmail.com>
2023-12-03 10:43:08 +0700

    Push grand design template mobile app

--- lib/screens/keranjang.dart
+++ lib/screens/keranjang.dart
@@ -8,18 +8,17 @@ class ShopItem {
   ShopItem(this.name, this.icon);
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
+class KeranjangPage extends StatelessWidget {
+  KeranjangPage({Key? key}) : super(key: key);
 
   final List<ShopItem> items = [
-    ShopItem("Lihat Produk", Icons.checklist),
-    ShopItem("Tambah Produk", Icons.add_shopping_cart),
-    ShopItem("Logout", Icons.logout),
+
   ];
 
   void _onSearch() {
     // Define what happens when the search icon is tapped.
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -92,41 +91,6 @@ class MyHomePage extends StatelessWidget {
 
         ),
       ),
-      drawer: const LeftDrawer(),
-      body: SingleChildScrollView(
-        // Widget wrapper yang dapat discroll
-        child: Padding(
-          padding: const EdgeInsets.all(10.0), // Set padding dari halaman
-          child: Column(
-            // Widget untuk menampilkan children secara vertikal
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
-                child: Text(
-                  'GEDE-Books (Placeholder)', // Text yang menandakan toko
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(102, 37, 73, 1.0)
-                  ),
-                ),
-              ),
-              // Grid layout
-              GridView.count(
-                // Container pada card kita.
-                primary: true,
-                padding: const EdgeInsets.all(20),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                crossAxisCount: 3,
-                shrinkWrap: true,
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
@@ -139,7 +103,6 @@ class ShopCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-
     );
   }
 }
--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gede_books/screens/menu.dart';
-import 'package:gede_books/screens/shoplist_form.dart';
+import 'package:gede_books/screens/keranjang.dart';
+import 'package:gede_books/screens/wishlist.dart';
+import 'package:gede_books/screens/order_history.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -8,52 +10,167 @@ class LeftDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.indigo,
-              ),
+      child: ListView( // Menggunakan ListView untuk memungkinkan item-itemnya scrollable
+        children: <Widget>[
+          Container(
+            color: Colors.grey[850],
+            height: 140,
+            child: Center(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'Shopping List',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/GEDE-Books Logo.png',
+                          width: 30,
+                          height: 30,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          'GEDE-Books',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Padding(padding: EdgeInsets.all(10)),
-                  Text("Catat seluruh keperluan belanjamu di sini!",
+                  Text(
+                    'Great Educational, Diverse,',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    '& Entertaining Books',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
                   ),
                 ],
               ),
             ),
+          ),
+          ExpansionTile(
+            leading: Icon(Icons.shelves),
+            title: Text('Kategori'),
+            children: <Widget>[
+              ListTile(
+                title: const Text('     Adventure'),
+                onTap: () {
+                  // Aksi ketika 'Adventure' dipilih
+                },
+              ),
+              ListTile(
+                title: const Text('     Children'),
+                onTap: () {
+                  // Aksi ketika 'Children' dipilih
+                },
+              ),
+              ListTile(
+                title: const Text('     Horror'),
+                onTap: () {
+                  // Aksi ketika 'Horror' dipilih
+                },
+              ),
+              ListTile(
+                title: const Text('     Humorous'),
+                onTap: () {
+                  // Aksi ketika 'Humorous' dipilih
+                },
+              ),
+              ListTile(
+                title: const Text('     Love'),
+                onTap: () {
+                  // Aksi ketika 'Love' dipilih
+                },
+              ),
+              ListTile(
+                title: const Text('     Movie'),
+                onTap: () {
+                  // Aksi ketika 'Movie' dipilih
+                },
+              ),
+              ListTile(
+                title: const Text('     Politics'),
+                onTap: () {
+                  // Aksi ketika 'Politics' dipilih
+                },
+              ),
+              ListTile(
+                title: const Text('     Sci-Fi'),
+                onTap: () {
+                  // Aksi ketika 'Sci-Fi' dipilih
+                },
+              ),
+              // Tambahkan lebih banyak ListTile di sini untuk kategori lain
+            ],
+          ),
           ListTile(
-            leading: const Icon(Icons.home_outlined),
-            title: const Text('Halaman Utama'),
-            // Bagian redirection ke MyHomePage
+            leading: Icon(Icons.home_outlined),
+            title: Text('Halaman Utama'),
             onTap: () {
               Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MyHomePage(),
-                  ));
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyHomePage(),
+                ),
+              );
             },
           ),
           ListTile(
-            leading: const Icon(Icons.add_shopping_cart),
-            title: const Text('Tambah Produk'),
-            // Bagian redirection ke ShopFormPage
+            leading: Icon(Icons.shopping_cart_outlined),
+            title: Text('Keranjang Saya'),
             onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ShopFormPage()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => KeranjangPage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.library_books_outlined),
+            title: Text('Pesanan Saya'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => OrderHistoryPage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.favorite_border),
+            title: Text('WishList Saya'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => WishlistPage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.person_outline),
+            title: Text('Login'),
+            onTap: () {
+              // Aksi ketika 'Login' dipilih
             },
           ),
         ],
       ),
-      );
+    );
   }
 }
