b8728c01dd042c89c2f3e0566de6e9f61e79ce07
rakbidb <rakbidb@gmail.com>
2023-12-20 19:09:29 +0700

    erasing some comments at wishlist_model.dart

--- lib/models/wishlist_model.dart
+++ lib/models/wishlist_model.dart
@@ -10,56 +10,40 @@ String wishlistItemToJson(WishlistItem data) => json.encode(data.toJson());
 
 class WishlistItem {
     List<WishlistItemElement> wishlistItems;
-    // int total;
 
     WishlistItem({
         required this.wishlistItems,
-        // required this.total,
     });
 
     factory WishlistItem.fromJson(Map<String, dynamic> json) => WishlistItem(
         wishlistItems: List<WishlistItemElement>.from(json["wishlist_items"].map((x) => WishlistItemElement.fromJson(x))),
-        // total: json["total"],
     );
 
     Map<String, dynamic> toJson() => {
         "wishlist_items": List<dynamic>.from(wishlistItems.map((x) => x.toJson())),
-        // "total": total,
     };
 }
 
 class WishlistItemElement {
     int id;
     String title;
-    // int quantity;
-    // int price;
-    // int totalPrice;
     String imageUrl;
 
     WishlistItemElement({
         required this.id,
         required this.title,
-        // required this.quantity,
-        // required this.price,
-        // required this.totalPrice,
         required this.imageUrl,
     });
 
     factory WishlistItemElement.fromJson(Map<String, dynamic> json) => WishlistItemElement(
         id: json["id"],
         title: json["title"],
-        // quantity: json["quantity"],
-        // price: json["price"],
-        // totalPrice: json["total_price"],
         imageUrl: json["image_url"],
     );
 
     Map<String, dynamic> toJson() => {
         "id": id,
         "title": title,
-        // "quantity": quantity,
-        // "price": price,
-        // "total_price": totalPrice,
         "image_url": imageUrl,
     };
 }
