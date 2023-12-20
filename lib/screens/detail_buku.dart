f9cd8e4f139d87d298d5aa878002a416aa91cb08
rakbidb <rakbidb@gmail.com>
2023-12-20 19:46:07 +0700

    fixing merge jadinya ke branch abid-new

--- lib/screens/detail_buku.dart
+++ lib/screens/detail_buku.dart
@@ -11,6 +11,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:gede_books/models/cart.dart';
+import 'package:gede_books/models/model_wishlist.dart';
 
 class BookDetailPage extends StatefulWidget {
   // final Book book;
@@ -85,8 +86,7 @@ class _BookDetailPageState extends State<BookDetailPage> {
                     builder: (context) => KeranjangPage(),
                   ),
                 );
-              }
-              else if (!request.loggedIn) {
+              } else if (!request.loggedIn) {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -116,17 +116,16 @@ class _BookDetailPageState extends State<BookDetailPage> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10.0), // Margin di sisi halaman
+          padding:
+              EdgeInsets.symmetric(horizontal: 10.0), // Margin di sisi halaman
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(height: 20),
-                Text(
-                  widget.title, // Title
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center
-                ),
+                Text(widget.title, // Title
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center),
                 SizedBox(height: 15), // Space between title and image
                 Image.asset(
                   widget.imagePath,
@@ -176,7 +175,9 @@ class _BookDetailPageState extends State<BookDetailPage> {
                             "Bahasa",
                             style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                          SizedBox(width: 8), // Add some space between "Bahasa" and the language name
+                          SizedBox(
+                              width:
+                                  8), // Add some space between "Bahasa" and the language name
                           Text(
                             "${widget.language.toString().split('.').last}", // Extract the language name from the enum
                             textAlign: TextAlign.center,
@@ -191,15 +192,20 @@ class _BookDetailPageState extends State<BookDetailPage> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text("Kategori:", style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text("Kategori:",
+                          style: TextStyle(fontWeight: FontWeight.bold)),
                       Text(
                         widget.category == " "
                             ? "Tidak ada informasi kategori"
                             : widget.category,
                         textAlign: TextAlign.center,
                         style: TextStyle(
-                          fontStyle: widget.category == " " ? FontStyle.italic : FontStyle.normal,
-                          color: widget.category == " " ? Colors.red : Colors.black,
+                          fontStyle: widget.category == " "
+                              ? FontStyle.italic
+                              : FontStyle.normal,
+                          color: widget.category == " "
+                              ? Colors.red
+                              : Colors.black,
                         ),
                       ),
                     ],
@@ -214,11 +220,11 @@ class _BookDetailPageState extends State<BookDetailPage> {
                         if (request.loggedIn) {
                           // TODO pass data add to cart
                           // endpoint: "https://lidwina-eurora-gedebooks.stndar.dev/add_to_cart/${widget.bookCode}"
-                          final response = await http.post(Uri.parse("https://lidwina-eurora-gedebooks.stndar.dev/add_to_cart/${widget.bookCode}"));
+                          final response = await http.post(Uri.parse(
+                              "https://lidwina-eurora-gedebooks.stndar.dev/add_to_cart/${widget.bookCode}"));
 
                           print(response.statusCode);
-                        }
-                        else if (!request.loggedIn) {
+                        } else if (!request.loggedIn) {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
@@ -234,7 +240,9 @@ class _BookDetailPageState extends State<BookDetailPage> {
                         );
                       },
                       style: ElevatedButton.styleFrom(
-                        primary: isCartPressed ? Colors.blue : Colors.grey[850], // Color change when pressed
+                        primary: isCartPressed
+                            ? Colors.blue
+                            : Colors.grey[850], // Color change when pressed
                         onPrimary: Colors.white,
                       ),
                       child: Row(
@@ -246,10 +254,62 @@ class _BookDetailPageState extends State<BookDetailPage> {
                     ),
                     SizedBox(width: 10), // Space between buttons
                     OutlinedButton(
-                      onPressed: () {
+                      onPressed: () async {
                         /*
                         TODO: Tambahkan logic untuk memasukkan ke wishlist di sini
                         */
+                        if (request.loggedIn) {
+                          try {
+                            // Membuat objek WishlistItemElement dari informasi buku
+                            WishlistItemElement booktoAddWishlist =
+                                WishlistItemElement(
+                              id: widget.bookCode,
+                              title: widget.title,
+                              imageUrl: widget.imagePath,
+                            );
+
+                            // Konversi objek WishlistItemElement ke JSON
+                            String objekJson =
+                                json.encode(booktoAddWishlist.toJson());
+
+                            // Mengirim permintaan POST dengan data JSON
+                            final response = await http.post(
+                              Uri.parse(
+                                  "https://lidwina-eurora-gedebooks.stndar.dev/add_to_wishlist/${widget.bookCode}"),
+                              headers: {"Content-Type": 'application/json'},
+                              body: objekJson,
+                            );
+
+                            print(response.statusCode);
+                            if (response.statusCode == 200) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                    content: Text(
+                                        'Buku ditambahkan ke wishlist! ^v^')),
+                              );
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                    content: Text(
+                                        'Gagal menambahkan buku ke wishlist! :(')),
+                              );
+                            }
+                          } catch (e) {
+                            print('Error: $e');
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                  content: Text(
+                                      'Terjadi kesalahan. Silakan coba lagi nanti')),
+                            );
+                          }
+                        } else if (!request.loggedIn) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ),
+                          );
+                        }
                         setState(() {
                           isWishListPressed = !isWishListPressed;
                         });
@@ -258,9 +318,19 @@ class _BookDetailPageState extends State<BookDetailPage> {
                         );
                       },
                       style: OutlinedButton.styleFrom(
-                        primary: isWishListPressed ? Colors.white : Colors.grey[850], // Text color change when pressed
-                        backgroundColor: isWishListPressed ? Colors.pink : Colors.white, // Background color change when pressed
-                        side: BorderSide(color: isWishListPressed ? Colors.pink : Color.fromARGB(255, 30, 30, 30)), // Border color change when pressed
+                        primary: isWishListPressed
+                            ? Colors.white
+                            : Colors
+                                .grey[850], // Text color change when pressed
+                        backgroundColor: isWishListPressed
+                            ? Colors.pink
+                            : Colors
+                                .white, // Background color change when pressed
+                        side: BorderSide(
+                            color: isWishListPressed
+                                ? Colors.pink
+                                : Color.fromARGB(255, 30, 30,
+                                    30)), // Border color change when pressed
                       ),
                       child: Row(
                         children: [
